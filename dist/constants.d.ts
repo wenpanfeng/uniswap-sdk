a4b6b61990f7ddde5e828bb26707b9fb9987f8fb
liubaoli <uoi00@qq.com>
2021-06-22 12:19:56 +0800

    api

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x99dD3fDf727D469E932220e000e89B92e5971843";
+export declare const INIT_CODE_HASH = "74e715a8d098397cb81d67353045ac6ff76aad7ccfcea321c0828e63e9b358f0";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
