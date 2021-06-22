d2a58aa959781fe3ff2bf143259f18166f7adad6
liubaoli <uoi00@qq.com>
2021-06-22 12:12:48 +0800

    first commi

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x99dD3fDf727D469E932220e000e89B92e5971843'
+
+export const INIT_CODE_HASH = '74e715a8d098397cb81d67353045ac6ff76aad7ccfcea321c0828e63e9b358f0'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
